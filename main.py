d35c94d36afc6cf22ed19096652c2bbecf7787c3
xvxvdee <deandra.spikemadden@ryerson.ca>
2021-02-25 16:21:03 -0500

    Updating commands

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ async def on_message(message):
     if message.content.startswith("&testing"):
         await message.channel.send(message)
 
-@bot.test()
+@bot.command()
 async def test(ctx, *args):
     await ctx.send(len(args))
 
